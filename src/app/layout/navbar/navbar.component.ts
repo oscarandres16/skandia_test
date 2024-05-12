54a83d957cfcaa7f0a884dadf44af71a23e8670f
Oscar Andres Gil Gamez <osangil16@gmail.com>
2024-05-12 15:37:02 -0500

    Implement navbar and footer responsive

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
@@ -20,6 +21,7 @@ import { MenuItem } from '../../interfaces/navbar.interface';
     MatDividerModule,
     MatSidenavModule,
     MatListModule,
+    MatMenuModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
