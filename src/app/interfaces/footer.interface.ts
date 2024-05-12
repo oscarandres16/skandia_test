2ba958431acb6592849cefe69e4abf04f46cd16e
Oscar Andres Gil Gamez <osangil16@gmail.com>
2024-05-12 15:10:18 -0500

    Implement footer

---
+++ src/app/interfaces/footer.interface.ts
@@ -0,0 +1,10 @@
+export interface Link {
+  name: string;
+  url: string;
+}
+
+export interface SocialMediaLink {
+  name: string;
+  url: string;
+  iconUrl: string;
+}
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Link, SocialMediaLink } from '../../interfaces/footer.interface';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  public links: Link[] = [];
+  public socialMediaLinks: SocialMediaLink[] = [];
 
+  ngOnInit(): void {
+    this.getLinks();
+    this.getSocialMediaLinks();
+  }
+
+  private getLinks(): void {
+    this.links = [
+      { name: 'Términos y Condiciones Canales de Servicio', url: '#' },
+      { name: 'Defensoría del Consumidor Financiero', url: '#' },
+      { name: 'Protección de Datos', url: '#' },
+      {
+        name: 'Definiciones Generales – Auto declaración FATCA y CRS',
+        url: '#',
+      },
+      { name: 'Recomendaciones de Seguridad', url: '#' },
+      { name: 'Ley de Transparencia Mapa del sitio', url: '#' },
+    ];
+  }
+
+  getSocialMediaLinks() {
+    this.socialMediaLinks = [
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/SkandiaCol',
+        iconUrl: '../../../assets/imgs/svgs/facebookl.svg',
+      },
+      {
+        name: 'Youtube',
+        url: 'https://www.youtube.com/channel/UCBkvnW9tFpaNZaNe8zbxPyg',
+        iconUrl: '../../../assets/imgs/svgs/youtube.svg',
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/SkandiaCol',
+        iconUrl: '../../../assets/imgs/svgs/twitter.svg',
+      },
+    ];
+  }
 }
