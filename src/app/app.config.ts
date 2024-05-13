8e1f3c63aadebcba4621ec1ab806955c327a407c
Oscar Andres Gil Gamez <osangil16@gmail.com>
2024-05-13 02:06:35 -0500

    Fix slider responsive

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
 import { provideProtractorTestingSupport } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideProtractorTestingSupport(),
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAnimationsAsync('animations'),
     importProvidersFrom(HttpClientModule),
   ],
 };
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,15 +4,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { CardComponent } from '../../components/card/card.component';
 import { Product } from '../../interfaces/product.interface';
 import { HomeService } from '../../services/home.service';
-
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    CarouselModule,
     RouterModule,
     MatButtonModule,
     MatCardModule,
@@ -25,6 +26,49 @@ import { HomeService } from '../../services/home.service';
 })
 export class HomeComponent implements OnInit {
   public products: Product[] = [];
+  public customOptions: OwlOptions = {
+    mouseDrag: true,
+    dots: false,
+    fluidSpeed: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      350: {
+        items: 1,
+      },
+      400: {
+        items: 1.3,
+      },
+      600: {
+        items: 1.5,
+      },
+      700: {
+        items: 1.8,
+      },
+      800: {
+        items: 2.2,
+      },
+      1000: {
+        items: 2.8,
+      },
+      1200: {
+        items: 3.2,
+      },
+      1400: {
+        items: 3.8,
+      },
+      1600: {
+        items: 4.5,
+      },
+      1800: {
+        items: 5,
+      }
+    },
+    nav: true,
+    navText: ['<', '>'],
+  };
   private currentSlide: number = 0;
 
   constructor(private homeService: HomeService) {}
